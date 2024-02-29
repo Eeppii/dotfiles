e68af13e87e0832ff771fc74cd19e4a18d2c4e0e
Thomas Eppenberger <thomas.eppenberger@sevensense.ch>
2024-02-29 11:14:23 +0100

    add telescope path truncation

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        -- Other good options: truncate, shorten, shorten = 2 (untested)
+        path_display = { "smart" },
+      },
+    },
+  },
+}
