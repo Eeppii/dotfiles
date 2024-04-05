18f89e904c8599bb4d1610e9eddaf583f99ff575
Thomas Eppenberger <thomas.eppenberger@sevensense.ch>
2024-04-05 10:24:38 +0200

    hardcode resolution in swap script as it does not work for tilted screens

--- scripts/swap.sh
+++ scripts/swap.sh
@@ -9,6 +9,9 @@ getwindowat() {
     echo $WINDOW
 }
 
+hardcoded_width=1440
+hardcoded_height=1413
+
 # get active app
 active=`xdotool getactivewindow`
 # get coordinates of an active app
@@ -17,11 +20,14 @@ eval `xdotool getwindowgeometry --shell $active`
 # if left border of an app is less than display width
 # (e.g. one display is 1920px wide, app has x = 200 - means it's 200px to the right from the left border of left monitor
 # if it has x = 1920 or more, it's on the right window), it's on screen 0, and we need to focus to screen 1, otherwise to screen 0
-(( $X >= $WIDTH )) && focustoscreen=0 || focustoscreen=1;
+#(( $X >= $WIDTH )) && focustoscreen=0 || focustoscreen=1;
+(( $X >= $hardcoded_width )) && focustoscreen=0 || focustoscreen=1;
 
 # get coordinates of the middle of the screen we want to switch
-searchx=$[ ($WIDTH / 2) + $focustoscreen * $WIDTH ]
-searchy=$[ $HEIGHT / 2 ]
+# searchx=$[ ($WIDTH / 2) + $focustoscreen * $WIDTH ]
+# searchy=$[ $HEIGHT / 2 ]
+searchx=$[ ($hardcoded_width / 2) + $focustoscreen * $hardcoded_width ]
+searchy=$[ $hardcoded_height / 2 ]
 
 # get window in that position
 window=`getwindowat $searchx $searchy`
