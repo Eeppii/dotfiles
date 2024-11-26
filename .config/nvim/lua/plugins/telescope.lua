7e437a1e37792e29d009cfcc35dc5e62f1046c24
Thomas Eppenberger <thomas.eppenberger@sevensense.ch>
2024-11-26 16:00:49 +0100

    add telescope with arguments

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,8 +4,27 @@ return {
     opts = {
       defaults = {
         -- Other good options: smart, truncate, shorten, shorten = 2 (does not work yet)
-        path_display = { "truncate" },
+        path_display = { "shorten" },
       },
     },
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      -- first setup telescope
+      telescope.setup({
+        -- your config
+      })
+
+      -- then load the extension
+      telescope.load_extension("live_grep_args")
+    end,
   },
 }
