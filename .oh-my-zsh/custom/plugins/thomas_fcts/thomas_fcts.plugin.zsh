8d2ce13d6d120b7ae8b35364cf61101f70a788d9
Thomas Eppenberger <thomas.eppenberger@sevensense.ch>
2025-02-19 09:43:43 +0100

    no longer needed config pulling

--- .oh-my-zsh/custom/plugins/thomas_fcts/thomas_fcts.plugin.zsh
+++ .oh-my-zsh/custom/plugins/thomas_fcts/thomas_fcts.plugin.zsh
@@ -15,25 +15,6 @@ function gcomn() {
   git commit -m "$1" -n
 }
 
-# 7s-configuration-client simple pull
-function confp() {
-  if [ "$#" -lt 4 ]; then
-    echo "Illegal number of arguments. Expected 4 (machine type, id, branch, and machine classes)."
-    return 1
-  fi
-
-  7s-configuration-client pull -t "$1" -i "$2" -b "$3" -c "$4" -f --ignore-generator-errors
-}
-
-function confp_legacy() {
-  if [ "$#" -lt 3 ]; then
-	  echo "Illegal number of arguments. Expected 3 (machine type, id, branch)."
-    return 1
-  fi
-
-  7s-configuration-client pull -t "$1" -i "$2" -b "$3" -f --ignore-generator-errors
-}
-
 function run_iwyu() {
     if [ -z "$1" ]; then
         echo "Not enough arguments"
